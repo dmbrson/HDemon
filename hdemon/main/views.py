d9a2cf2997ff4dbe6c06f2cb6c00af4bee2a01cb
dmbrson <dka04@list.ru>
2024-04-14 22:22:57 +1000

    Дописывание логики работы жанров

--- hdemon/main/views.py
+++ hdemon/main/views.py
@@ -19,4 +19,15 @@ def login(request):
 def show_film(request, film_id):
     return HttpResponse(f"Отображение фильма с id = {film_id}")
 def show_category(request, cat_id):
+    films = Films.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    context = {
+        'films': films,
+        'cats': cats,
+        'cat_selected': cat_id,
+    }
+
+    return render(request,  'main/index.html', context=context)
+
     return HttpResponse(f"Отображение жанра с id = {cat_id}")
